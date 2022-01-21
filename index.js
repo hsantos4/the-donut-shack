5fd5496a592ae0b5f6ae93dc75b70715dc6232e5
hsantos4 <78284173+hsantos4@users.noreply.github.com>
2022-01-21 15:51:52 -0500

    Completed the hero section, started on the image gallery, but will need to work on the javascript to make it work. Started on the process section, did it a lot of styles

--- index.js
+++ index.js
@@ -2,13 +2,47 @@
 
 const mobileBtn = document.querySelector('.mobile-btn');
 const mobileItems = document.querySelector('.mobile-items');
+const mobileContainer = document.querySelector('.mobile-container')
+const navHeader = document.querySelector('.nav-header');
+const mainNav = document.querySelector('.main-nav');
 
-// This adds 
+const backBtn = document.querySelector('.back-btn');
+const nextBtn = document.querySelector('.next-btn');
+const imgArr = [0, 1, 2, 3, 4, 5];
+
+// Mobile click down
 mobileBtn.addEventListener('click', () =>  {
-    if (mobileItems.classList.contains('hidden')) {
-        mobileItems.classList.remove('hidden')
+    if (mobileContainer.classList.contains('hidden')) {
+        mobileContainer.classList.remove('hidden')
     } else {
-        mobileItems.classList.add('hidden');
+        mobileContainer.classList.add('hidden');
     }
-    
-})
+});
+// This is the end of drop down menu
+
+
+
+// This is going to make the navigation stick to the top of th document
+let options = {
+    root: null,
+    threshold: 0
+}
+const headerObserver = (entries) => {
+     if(!entries.isIntersecting) {
+        navHeader.classList.add('sticky')
+    } else {
+        navHeader.classList.remove('sticky')
+    }
+    console.log(entries)
+}
+
+const observer = new IntersectionObserver(headerObserver, options);
+observer.observe(mainNav);
+// This is the end of the sticky navigation
+
+// 
+
+// This will make a random number that help determine what image is shown
+function getRandomNumber() {
+    return Math.floor(Math.random() * 6);
+}
